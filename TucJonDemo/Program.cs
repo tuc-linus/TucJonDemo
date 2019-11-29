f0909074b23ec1c5e736fbeafdb552d2693fd4ab
Linus Rudbeck <linus.rudbeck+tuc@redcapes.se>
2019-11-29 14:11:06 +0100

    Chenged user actions

--- TucJonDemo/Program.cs
+++ TucJonDemo/Program.cs
@@ -6,7 +6,12 @@ namespace TucJonDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hej!");
+            Console.WriteLine("Vad heter du?");
+            Console.Write("Svara: ");
+
+            string name = Console.ReadLine();
+            Console.WriteLine($"Du heter {name}");
         }
     }
 }
