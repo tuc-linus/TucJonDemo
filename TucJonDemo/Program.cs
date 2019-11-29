b79bcf8ba1b930bf1abd613253321c163e220b1d
Linus Rudbeck <linus.rudbeck+tuc@redcapes.se>
2019-11-29 14:03:40 +0100

    Added project

---
+++ TucJonDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TucJonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
